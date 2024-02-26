ac613c71689c9430a0cc8cc4e23bdcb462ed8e99
Krzysztof Szwajka <krzysztof.szwajka@student.wsg.pl>
2024-02-26 14:06:21 +0100

    Dodanie możliowści zapisu i odczytu hasła do pliku. Dodanie właściwości StatusMessage i ErrorMessage

--- ToolsKS/ToolHash.cs
+++ ToolsKS/ToolHash.cs
@@ -7,7 +7,9 @@ namespace ToolsKS
     {
         public string? HashedPassword {  get; set; }
         public string? Password { get; set; }
-        public string PasswordHash(string password)
+        public string? ErrorMessage {  get; set; }
+        public string? StatusMessage {  get; set; }
+        public string PasswordToHash(string password)
         {
             using(var sha256 = SHA256.Create() )
             {
@@ -19,13 +21,41 @@ namespace ToolsKS
 
         public bool VerifyPassword(string password, string passwordHash)
         {
-            var hash = PasswordHash(password);
+            var hash = PasswordToHash(password);
             return hash == passwordHash;
         }
 
+        public void SavePasswordToFile(string password, string pathFile)
+        {
+            if (!File.Exists(pathFile))
+                File.Create(pathFile).Close();
+
+            using (var writer = new StreamWriter(pathFile, true))
+            {
+                writer.WriteLine(password);
+            }
+            this.StatusMessage = "Zapisano z powodzeniem.";
+        }
+
+        public void LoadPasswordFromFile(string pathFile)
+        {
+            if (File.Exists(pathFile))
+            {
+                using (var reader = new StreamReader(pathFile))
+                {
+                    this.HashedPassword = reader.ReadToEnd();
+                }
+                this.StatusMessage = "Odczyt poprawny";
+            }
+            else
+            {
+                this.ErrorMessage = "Brak pliku do odczytu";
+                this.StatusMessage = "brak";
+            }
+        }
         public string Info() 
         {
-            return "Dll Version: 1.00\n" +
+            return "Dll Version: 2.00\n" +
                 "Autor:       Krzysztof Szwajka\n" +
                 "Tel.:        +48 785 192 785\n" +
                 "e-mail:      k.szwajka@gmail.com";
